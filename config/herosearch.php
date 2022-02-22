356aca28d84503f06bbb7c8709e1851605b51538
Phạm Minh Đạt <dat.pham@carro.co>
2022-02-22 15:17:53 +0700

    MOD Readme + Missing config

--- config/herosearch.php
+++ config/herosearch.php
@@ -2,5 +2,6 @@
 
 return [
     'host'  => env('ELASTICSEARCH_HOST', '127.0.0.1'),
-    'port'  => env('ELASTICSEARCH_PORT', '9200')
+    'port'  => env('ELASTICSEARCH_PORT', '9200'),
+    'index_prefix' => env('ELASTICSEARCH_INDEX_PREFIX', null)
 ];
